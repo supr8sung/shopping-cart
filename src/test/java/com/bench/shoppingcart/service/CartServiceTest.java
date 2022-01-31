8ba0c9bb8a6f5aa9c15353e4fcc05487e573bf20
Vinay Garg <Vinaygarg712@gmail.com>
2022-01-31 22:01:44 +0530

    test cases get and delete

--- src/test/java/com/bench/shoppingcart/service/CartServiceTest.java
+++ src/test/java/com/bench/shoppingcart/service/CartServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -21,14 +25,30 @@ class CartServiceTest {
     @Mock
     private CartRepository cartRepository;
 
-
     @Test
     void should_be_able_to_add_an_item() {
-
         Item item = Item.builder().withName("item1").withPrice(4500d).build();
         Item savedItem = Item.builder().withId(1).withName("item2").withPrice(4500d).build();
         when(cartRepository.save(any())).thenReturn(savedItem);
         Item response = cartService.add(item);
         Assertions.assertEquals("item_saved", response.getName());
+    }
+
+    @Test
+    void should_be_able_to_delete_an_item() {
+        Item item = Item.builder().withId(1).withName("item_test").withPrice(300d).build();
+        cartService.deleteById(item.getId());
+    }
+
+    @Test
+    void should_be_able_to_fetch_all_items(){
+        Item item1 = Item.builder().withId(1).withName("item_test").withPrice(300d).build();
+        Item item2 = Item.builder().withId(2).withName("item_test").withPrice(300d).build();
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+        when(cartRepository.findAll()).thenReturn(items);
+        ArrayList<Item> itemsResponse = (ArrayList<Item>) cartService.getAll();
+        Assertions.assertEquals(2,itemsResponse.size());
     }
 }
