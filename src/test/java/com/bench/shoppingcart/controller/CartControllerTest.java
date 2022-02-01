fb85330380473b40b9a12f26348125ef462a3e43
singh1supre718 <supreet.singh@telekom-digital.com>
2022-02-02 01:40:20 +0530

    fixing test cases

--- src/test/java/com/bench/shoppingcart/controller/CartControllerTest.java
+++ src/test/java/com/bench/shoppingcart/controller/CartControllerTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Profile("test")
+@ActiveProfiles("test")
 class CartControllerTest {
 
     @Autowired
